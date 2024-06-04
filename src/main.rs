bb7c391619ee5b53fe73f946e37dd9909e0888c3
sol <sol.recieve@gmail.com>
2024-06-04 18:09:22 +0200

    feat: add message in/out

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,46 @@
 use anyhow::Result;
 use axum::routing::get;
-use socketioxide::extract::SocketRef;
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+use socketioxide::extract::{Data, SocketRef};
 use socketioxide::SocketIo;
+use tower::ServiceBuilder;
+use tower_http::cors::CorsLayer;
 use tracing::info;
+
+#[derive(Debug, Deserialize)]
+struct MessageIn {
+    room: String,
+    text: String,
+}
+
+#[derive(Debug, Serialize)]
+struct MessageOut {
+    text: String,
+    user: String,
+    date: DateTime<Utc>,
+}
 
 async fn on_connect(socket: SocketRef) {
     info!("socket connected: {:?}", socket.id);
+
+    socket.on("join", |socket: SocketRef, Data::<String>(room)| {
+        info!("join received {:?}", room);
+        let _ = socket.leave_all();
+        let _ = socket.join(room);
+    });
+
+    socket.on("message", |socket: SocketRef, Data::<MessageIn>(data)| {
+        info!("message received {:?}", data);
+
+        let response = MessageOut {
+            text: data.text,
+            user: format!("anon-{}", socket.id),
+            date: Utc::now(),
+        };
+
+        let _ = socket.within(data.room).emit("message", response);
+    });
 }
 
 #[tokio::main]
@@ -17,7 +52,11 @@ async fn main() -> Result<()> {
 
     let router = axum::Router::new()
         .route("/", get(|| async { "Hello, World!" }))
-        .layer(layer);
+        .layer(
+            ServiceBuilder::new()
+                .layer(CorsLayer::permissive())
+                .layer(layer),
+        );
 
     info!("starting server");
 
